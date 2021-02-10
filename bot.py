052f096fb7361d7d1f0e4ad50ecc86328f48fe84
iiiiii1wepfj <18629298+iiiiii1wepfj@users.noreply.github.com>
2021-02-10 14:53:15 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,7 +9,7 @@ app = Client("antiviewcounterbot", api_id, api_hash, bot_token=token)
 
 viewsfilter = filters.create(lambda _, __, Message: Message.views)
 
-@app.on_message(viewsfilter & ~filters.linked_channel)
+@app.on_message(viewsfilter & ~filters.linked_channel & ~filters.channel)
 async def antiviewcounter(client, message):
  await message.delete()
 
