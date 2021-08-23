e677c65275501dcf2a1ea5bcffe834e370059aa6
iiiiii1wepfj <18629298+iiiiii1wepfj@users.noreply.github.com>
2021-08-23 12:53:02 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,15 +2,17 @@ from pyrogram import Client, filters
 from pyrogram.types import Message
 
 api_id = api id
-api_hash = 'api hash'
+api_hash = "api hash"
 token = "token"
 
 app = Client("antiviewcounterbot", api_id, api_hash, bot_token=token)
 
 viewsfilter = filters.create(lambda _, __, Message: Message.views)
 
+
 @app.on_message(viewsfilter & ~filters.linked_channel & ~filters.channel)
 async def antiviewcounter(client, message):
- await message.delete()
+    await message.delete()
+
 
 app.run()
